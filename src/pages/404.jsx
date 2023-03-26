b4c199137f1ff0e709e4c5bb025df4d39bf23ddd
Phillip Pargmann <pargmann92@gmail.com>
2023-03-26 11:48:35 +0200

    change 404 page and anlyze var

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,22 +3,21 @@ import React from "react";
 import Link from "next/link";
 
 import Container from "@/components/Container";
-import Layout from "@/components/Layout";
 
 const FourOhFour = () => {
   return (
-    <Layout>
+    <>
       <Container
         className={"min-h-[80vh] flex justify-center items-center flex-col"}
       >
         <h1>404 - Page Not Found</h1>
         <Link className="mt-4" href="/">
           <p className="px-4 py-2 text-white rounded-full bg-f-red">
-            Go back home
+            Zurück zur Startseite
           </p>
         </Link>
       </Container>
-    </Layout>
+    </>
   );
 };
 
