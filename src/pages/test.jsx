fc1b39174f7f7cd76e710b3a5a8b6688d89e097f
Phillip Pargmann <pargmann92@gmail.com>
2023-04-15 14:51:49 +0200

    added test page for form submissions

---
+++ src/pages/test.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+import Container from "@/components/Container";
+import Layout from "@/components/Layout";
+
+const Test = () => {
+  return (
+    <Layout>
+      <Container>
+        <div className="flex min-h-full">
+          <div className="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div className="w-full max-w-sm mx-auto lg:w-96">
+              <div>
+                <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+                  Newsletter
+                </h2>
+              </div>
+
+              <div className="mt-8">
+                <div className="mt-6">
+                  <form
+                    name="contact"
+                    method="POST"
+                    data-netlify="true"
+                    className="space-y-6"
+                  >
+                    <div>
+                      <div className="space-y-1">
+                        <label
+                          htmlFor="name"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Name
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            id="name"
+                            name="name"
+                            type="name"
+                            autoComplete="current-name"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-f-red sm:text-sm sm:leading-6"
+                          />
+                        </div>
+
+                        <label
+                          htmlFor="email"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Email address
+                        </label>
+                        <div className="mt-2">
+                          <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-f-red sm:text-sm sm:leading-6"
+                          />
+                        </div>
+                      </div>
+                    </div>
+
+                    <div>
+                      <button
+                        type="submit"
+                        className="flex justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-f-red hover:bg-f-red/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-f-red"
+                      >
+                        Anmelden
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="relative flex-1 hidden w-0 lg:block">image</div>
+        </div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Test;
