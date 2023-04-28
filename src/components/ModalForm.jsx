7840dc00e16dd55eacdd0d54c2aa4223a31f2323
Phillip Pargmann <pargmann92@gmail.com>
2023-04-28 06:14:47 +0200

    added blose button for modal

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -55,6 +55,12 @@ function NewsletterSignUp({ setVisible }) {
   return (
     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="relative px-6 py-24 overflow-hidden shadow-2xl bg-f-green isolate sm:rounded-3xl sm:px-24 xl:py-32">
+        <div
+          className="absolute p-2 cursor-pointer top-4 right-4"
+          onClick={() => setVisible(false)}
+        >
+          <CloseIcon />
+        </div>
         <h2 className="max-w-2xl mx-auto text-3xl font-bold tracking-tight text-center text-white sm:text-4xl">
           Bleib auf dem Laufenden!
         </h2>
@@ -123,3 +129,18 @@ function NewsletterSignUp({ setVisible }) {
     </div>
   );
 }
+
+const CloseIcon = () => (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 10.9163L2.29278 17.6236C2.04182 17.8745 1.72243 18 1.3346 18C0.946768 18 0.627376 17.8745 0.376425 17.6236C0.125475 17.3726 0 17.0532 0 16.6654C0 16.2776 0.125475 15.9582 0.376425 15.7072L7.08365 9L0.376425 2.29278C0.125475 2.04182 0 1.72243 0 1.3346C0 0.946768 0.125475 0.627376 0.376425 0.376425C0.627376 0.125475 0.946768 0 1.3346 0C1.72243 0 2.04182 0.125475 2.29278 0.376425L9 7.08365L15.7072 0.376425C15.9582 0.125475 16.2776 0 16.6654 0C17.0532 0 17.3726 0.125475 17.6236 0.376425C17.8745 0.627376 18 0.946768 18 1.3346C18 1.72243 17.8745 2.04182 17.6236 2.29278L10.9163 9L17.6236 15.7072C17.8745 15.9582 18 16.2776 18 16.6654C18 17.0532 17.8745 17.3726 17.6236 17.6236C17.3726 17.8745 17.0532 18 16.6654 18C16.2776 18 15.9582 17.8745 15.7072 17.6236L9 10.9163Z"
+      fill="white"
+    />
+  </svg>
+);
