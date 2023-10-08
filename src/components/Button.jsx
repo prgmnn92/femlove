19770985285e5860498d6466cba9fec72cafa5f7
Phillip Pargmann <pargmann92@gmail.com>
2023-10-08 16:54:33 +0700

    transition state changed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ title, small, isSecondary, onClick, type = "button" }) => {
     <button
       onClick={onClick}
       type={type}
-      className={`relative  duration-300 rounded-[14px] transition-all focus:outline-none focus:ring-2 focus:ring-white/10 focus:ring-offset-2  border-[1px] border-solid border-f-brown cursor-pointer 
+      className={`relative  duration-300 rounded-[14px] transition-background focus:outline-none focus:ring-2 focus:ring-white/10 focus:ring-offset-2  border-[1px] border-solid border-f-brown cursor-pointer 
       ${small ? "px-3 py-1 text-xs" : "px-5 py-2 text-sm"}
       ${
         isSecondary
